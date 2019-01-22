0caaa6a9e3c6a48de27e9853319f63903a10e558
dingwanshun <dingwanshun@pateo.com.cn>
2019-01-22 16:38:05 +0800

    Initial commit

---
+++ app/src/main/java/com/hd/myaidlone/AIDLService.java
@@ -0,0 +1,105 @@
+package com.hd.myaidlone;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.RemoteCallbackList;
+import android.os.RemoteException;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Author: dingwanshun
+ * Time: 2019/1/22
+ * Description:
+ */
+public class AIDLService extends Service {
+    private static final String TAG = "AIDLService";
+    public static final int WHAT_MSG = 0x0010;
+    private RemoteCallbackList<IDemandListener> demandList = new RemoteCallbackList<>();
+    private int count;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            if (demandList != null) {
+                int nums = demandList.beginBroadcast();
+                for (int i = 0; i < nums; i++) {
+                    MessageBean messageBean = new MessageBean();
+                    messageBean.setContent("我丢");
+                    messageBean.setLevel(count);
+                    count++;
+                    try {
+                        demandList.getBroadcastItem(i).onDemandReceiver(messageBean);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+                demandList.finishBroadcast();
+            }
+
+            mHandler.sendEmptyMessageDelayed(WHAT_MSG, 3000);//每3s推一次消息
+        }
+    };
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return demandManager;
+    }
+
+    IDemandManager.Stub demandManager = new IDemandManager.Stub() {
+        @Override
+        public MessageBean getDemand() throws RemoteException {
+            MessageBean messageBean = new MessageBean();
+            messageBean.setContent("首先，看到我要敬礼");
+
+            return messageBean;
+        }
+
+        ////客户端数据流向服务端
+        @Override
+        public void setDemandIn(MessageBean msg) throws RemoteException {
+            Log.i(TAG, "程序员:" + msg.toString());
+        }
+
+        //服务端数据流向客户端
+        @Override
+        public void setDemandOut(MessageBean msg) throws RemoteException {
+            Log.i(TAG, "程序员:" + msg.toString());//msg内容一定为空
+
+            msg.setContent("我不想听解释，下班前把所有工作都搞好！");
+            msg.setLevel(5);
+        }
+
+        @Override
+        public void setDemandInOut(MessageBean msg) throws RemoteException {
+            Log.i(TAG, "程序员:" + msg.toString());
+
+            msg.setContent("把用户交互颜色都改成粉色");
+            msg.setLevel(3);
+        }
+
+        @Override
+        public void registerListener(IDemandListener listener) throws RemoteException {
+            if (demandList.register(listener)) {
+                mHandler.sendEmptyMessage(WHAT_MSG);
+            }
+        }
+
+        @Override
+        public void unregisterListener(IDemandListener listener) throws RemoteException {
+            if (demandList.unregister(listener)) {
+                mHandler.removeMessages(WHAT_MSG);
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
